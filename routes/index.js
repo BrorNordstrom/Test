acf9afde66fc76ff721f5f65b33cde6d2c8dae3e
EC2 Default User <ec2-user@ip-172-31-38-253.us-east-2.compute.internal>
2019-04-07 12:37:31 +0000

    Fixed

--- routes/index.js
+++ routes/index.js
@@ -11,26 +11,26 @@ router.get('/', function(req, res, next) {
    url = url+city+"&"+appId;
 request(url, function (error, response, body) {
       body = JSON.parse(body);
-      console.log(body);
       if(error && response.statusCode != 200){
         throw error;
       }
-    let country = (body.sys.country) ? body.sys.country : '' ;
     let forecast = "for "+city;
     res.render('index', {body : body, forecast: forecast});
    });
 });
 
 router.post('/weather', function(req, res, next){
+   url    = 'http://api.openweathermap.org/data/2.5/weather?q='
+  appId  = 'appid=b7df2b21211ac075ad73e8c0e0c1a4b1';
   let city = req.body.city;
   url = url+city+"&"+appId;
+  //eval(require("locus"));
  request(url, function (error, response, body) {
       body = JSON.parse(body);
       console.log(body);
       if(error && response.statusCode != 200){
         throw error;
       }
-    let country = (body.sys.country) ? body.sys.country : '' ;
     let forecast = "for "+city;
     res.render('index', {body : body, forecast: forecast});
    });
